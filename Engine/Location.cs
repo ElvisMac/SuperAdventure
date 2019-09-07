c81f0745c6406a366a8526b8224f0b4f5e6f7bb5
Elvis McIlmoyle <elvismac@outlook.com>
2019-09-07 21:08:14 +0300

    Added the World.cs code provided by Scott Lily.  Would have preferred that he had me go through this and enter the code myself rather than a copy/pasta.

--- Engine/Location.cs
+++ Engine/Location.cs
@@ -12,10 +12,10 @@ namespace Engine
         public Item ItemRequiredToEnter { get; set; }
         public Quest QuestAvailableHere { get; set; }
         public Monster MonsterLivingHere { get; set; }
-        public Location LocattionToNorth { get; set; }
-        public Location LocattionToEast { get; set; }
-        public Location LocattionToSouth { get; set; }
-        public Location LocattionToWest { get; set; }
+        public Location LocationToNorth { get; set; }
+        public Location LocationToEast { get; set; }
+        public Location LocationToSouth { get; set; }
+        public Location LocationToWest { get; set; }
 
 
         public Location(int id, string name, string description, Item itemRequiredToEnter = null, Quest questAvailableHere = null, Monster monsterLivingHere = null)
